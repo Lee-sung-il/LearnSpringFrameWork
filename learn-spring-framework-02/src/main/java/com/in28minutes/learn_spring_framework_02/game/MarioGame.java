58e9443b7c51909ee2733d8de7d052fe3d73b60b
Lee-sung-il <wj132612@naver.com>
2025-10-21 23:51:02 +0900

    Java 객체의 생성 및 관리를 위한 SpringFrameWork 이해하기

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework_02/game/MarioGame.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learn_spring_framework_02.game;
+
+import com.in28minutes.learn_spring_framework_02.game.GamingConsole;
+
+public class MarioGame implements GamingConsole {
+	
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	public void down() {
+		System.out.println("Go into a hole");
+	}
+	
+	public void left() {
+		System.out.println("Go back");
+	}
+
+	public void right() {
+		System.out.println("Accelerate");
+	}
+
+
+}
---
+++ learn-spring-framework-02/src/test/java/com/in28minutes/learn_spring_framework_02/LearnSpringFramework02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_spring_framework_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFramework02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
